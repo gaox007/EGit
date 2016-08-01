4cb8c3bdbf6113a16f9f0111fe9862a8e89faabb
gaox007 <gaoxiang581@163.com>
2016-08-01 22:22:02 +0800

    233
    323

--- src/com/atguigu/test/Test01.java
+++ src/com/atguigu/test/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 	
 	public static void main(String[] args) {
 		System.out.println("本地库上传到 github");
-		System.out.println("github更新的部分1");
+		System.out.println("github更新的部分new");
 	}
 
 }
